5ac47add9178a5fcfebd9e5e7c8b9ff2a0a98029
Paulaspalvina <112925837+Paulaspalvina@users.noreply.github.com>
2022-10-14 19:49:14 +0300

    Add files via upload

---
+++ kods dialogam ar lietotāju.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+//mainiigie
+    long long int a, b, c;
+//pirmais mainiigais
+    printf("Ievadi 400000: \n");
+    scanf("%lli", &a); 
+//otrais mainiigais
+    printf("Ievadi 100000: \n");
+    scanf("%lli", &b); 
+//reizinaajums
+    c = a * b;
+//rezultaats
+    printf("\n%lli", c,"\n");
+
+    return 0;
+}
